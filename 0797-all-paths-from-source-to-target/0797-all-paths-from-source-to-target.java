45199f657791fa82ee0a6c349a64dd56e6a88d05
Harshitha <harshinani9441@gmail.com>
2026-08-18 14:24:48 +0530

    Time: 1 ms (100%), Space: 47.9 MB (59.49%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+       List<List<Integer>> res = new ArrayList<>();
+       List<Integer> path = new ArrayList<>();
+       path.add(0);
+       solve(graph,res,path,0);
+       return res;
+    }
+    public void solve(int graph[][],List<List<Integer>> res,List<Integer> path,int start){
+        if(start == graph.length-1){
+            res.add(new ArrayList(path));  
+            return;
+        }
+        for(int ni:graph[start]){
+            path.add(ni);
+            solve(graph,res,path,ni);
+            path.remove(path.size()-1);
+        } 
+    }
+}
