c48f9c68a29cc872c8648eee5a0604cd7a778076
Harshitha <harshinani9441@gmail.com>
2026-09-25 21:11:06 +0530

    Time: 648 ms (68.12%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT
+    e.name,
+    b.bonus
+FROM Employee as e
+LEFT JOIN Bonus as b
+on e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL
+
